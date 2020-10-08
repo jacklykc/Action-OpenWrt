d850103ea64697f5c0f6c8edee3ff33564dcf720
jacklykc <46422230+jacklykc@users.noreply.github.com>
2020-10-08 18:10:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,8 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git jerrykuku https://github.com/jerrykuku/luci-theme-argon.git' feeds.conf.default
+sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-package' feeds.conf.default
+sed -i '$a src-git siropboy https://github.com/siropboy/mypackages' feeds.conf.default
+sed -i '$a src-git vernesong https://github.com/vernesong/OpenClash' feeds.conf.default
